2ac648722dd42dbbca86ba92f8018c30c1dd6429
Adisa Taiwo <adisataiwojoshua@gmail.com>
2019-04-17 16:37:54 +0100

    Working with HTML 5, CSS3 and Bootstrap 4 with a bit of jQuery

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+//About Us Accordion
+var acc = document.querySelectorAll(".accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+// smooth scrolling
+$('.nav-item a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+      const hash = this.hash;
+  
+      $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      )
+    }
+  });
